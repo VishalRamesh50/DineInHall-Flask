6b1b94b2a42114bb59c75040fb151e3b86179391
Vishal Ramesh <vishalramesh@Vishals-MacBook-Pro.local>
2019-04-07 18:14:11 -0400

    Initial Commit

---
+++ dineinhall/main/routes.py
@@ -0,0 +1,80 @@
+from flask import render_template, request, Blueprint, redirect, url_for, flash
+from sqlalchemy.sql import text
+from sqlalchemy import create_engine
+import os
+try:
+    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]  # TOKEN from Heroku
+except Exception:    
+    from ..creds import SQLALCHEMY_DATABASE_URI  # local TOKEN
+from .forms import SearchForm
+
+main = Blueprint('main', __name__)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+
+@main.route("/")
+def mainPage():
+    return render_template('main.html')
+
+
+@main.route("/menu")
+def home():
+    # page = request.args.get('page', 1, type=int)
+    # posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return redirect(url_for('main.filteredLocations', loc='IV'))
+
+@main.route("/menu/<loc>")
+def filteredLocations(loc):
+    with engine.connect() as con:
+        rs = con.execute('select distinct food_name, calories, protein, total_fat, total_carbs, meal_type '
+            + 'from menu join food_on_menu using (menu_id) '
+            + ' join food using (food_id) where menu_date '
+            + "= curdate() and location like '{}'".format(loc))
+
+    foods = list(rs)
+    empty = len(foods) == 0
+    locations = {'Stwest': False, 'IV': False, 'Steast': False}
+    locations[loc] = True
+
+    return render_template('menu.html', allFoods=foods, stwest=locations['Stwest'], iv=locations['IV'], steast=locations['Steast'], empty=empty, title=loc)
+
+@main.route("/AdvancedSearch", methods=['GET', 'POST'])
+def search():
+    form = SearchForm()
+    if form.validate_on_submit():
+        iv = form.iv.data
+        steast = form.steast.data
+        stwest = form.stwest.data
+        calories = form.calories.data
+        protein = form.protein.data
+        fat = form.fat.data
+        carbs = form.carbs.data
+        meal = form.meal.data
+        # flashMsg = str(iv) + ' ' + str(steast) + ' ' + str(stwest) + ' ' + str(calories) + ' ' + str(protein)
+        # flash(flashMsg, 'success')
+        iv = "location like 'IV'" if iv else False
+        steast = "location like 'Steast'" if steast else False
+        stwest = "location like 'Stwest'" if stwest else False
+        calories = "calories <= {}".format(calories) if calories != None else True
+        protein = "protein <= {}".format(protein) if protein != None else True
+        fat = "total_fat <= {}".format(fat) if fat != None else True
+        carbs = "total_carbs <= {}".format(carbs) if carbs != None else True
+        
+        with engine.connect() as con:
+            foods = con.execute('select distinct food_name, calories, protein, total_fat, total_carbs, location, meal_type '
+                + 'from menu join food_on_menu using (menu_id) '
+                + ' join food using (food_id) where menu_date '
+                + "= curdate() and ({} or {} or {}) ".format(iv, steast, stwest)
+                + "and {} and {} and {} and {} ".format(calories, protein, fat, carbs)
+                + "and meal_type like '{}' ".format(meal)
+                + "order by location desc, meal_type asc, calories desc")
+        foods = list(foods)
+        size = len(foods)
+        if size==0:
+            flash('No items matched with your query', 'danger')
+        else:
+            flash(f'Found {size} matching results!', 'success')
+    else:
+        foods = []
+        flash('Start Searching!', 'success')
+    return render_template('advancedSearch.html', title='Advanced Search', form=form, allFoods=foods)
---
+++ dineinhall/models.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+from dineinhall import db, login_manager
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    # posts = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Food(db.Model):
+    food_id = db.Column(db.Integer, primary_key=True)
+    food_name = db.Column(db.String(100), nullable=True)
+    serving = db.Column(db.Integer, nullable=True)
+    calories = db.Column(db.Integer, nullable=True)
+    calories_from_fat = db.Column(db.Integer, nullable=True)
+    cholesterol = db.Column(db.Integer, nullable=True)
+    dietary_fiber = db.Column(db.Integer, nullable=True)
+    protein = db.Column(db.Integer, nullable=True)
+    saturated_fat = db.Column(db.Integer, nullable=True)
+    sodium = db.Column(db.Integer, nullable=True)
+    sugar = db.Column(db.Integer, nullable=True)
+    total_carbs = db.Column(db.Integer, nullable=True)
+    total_fat = db.Column(db.Integer, nullable=True)
+    trans_fat = db.Column(db.Integer, nullable=True)
+    vitamin_d = db.Column(db.Integer, nullable=True)
+    vegetarian = db.Column(db.Boolean, nullable=False)
+    vegan = db.Column(db.Boolean, nullable=False)
+    balanced = db.Column(db.Boolean, nullable=False)
+
+class Menu(db.Model):
+    menu_id = db.Column(db.Integer, primary_key=True)
+    meal_type = db.Column(db.Enum('breakfast', 'lunch', 'dinner'), nullable=False)
+    location = db.Column(db.Enum('Stwest', 'Steast', 'IV'), nullable=True)
+    menu_date = db.Column(db.DateTime, nullable=True)
+
+class FoodOnMenu(db.Model):
+    # id = db.Column(db.Integer, primary_key=True)
+    food_id = db.Column(db.Integer, db.ForeignKey('menu.menu_id'), primary_key=True, nullable=False)
+    menu_id = db.Column(db.Integer, db.ForeignKey('food.food_id'), primary_key=True, nullable=False)
