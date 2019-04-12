7e6ecb37f85a9cd47ee5b66908c2e0cc000a2793
Vishal Ramesh <vishalramesh50@gmail.com>
2019-04-12 13:47:53 -0400

    Replaced formats with f-strings

--- dineinhall/main/routes.py
+++ dineinhall/main/routes.py
@@ -5,9 +5,9 @@ from datetime import datetime
 from pytz import timezone
 import os
 try:
-    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]  # TOKEN from Heroku
+    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]  # URI from Heroku
 except Exception:    
-    from ..creds import SQLALCHEMY_DATABASE_URI  # local TOKEN
+    from ..creds import SQLALCHEMY_DATABASE_URI  # local URI
 from .forms import SearchForm
 
 main = Blueprint('main', __name__)
@@ -27,9 +27,9 @@ def home():
 def filteredLocations(loc):
     with engine.connect() as con:
         rs = con.execute('select distinct * '
-            + 'from menu join food_on_menu using (menu_id) '
-            + ' join food using (food_id) where menu_date '
-            + "= curdate() and location like '{}'".format(loc))
+            'from menu join food_on_menu using (menu_id) '
+            ' join food using (food_id) where menu_date '
+            f"= curdate() and location like '{loc}'")
 
     foods = list(rs)
     empty = len(foods) == 0
@@ -62,21 +62,21 @@ def search():
         iv = "location like 'IV'" if iv else False
         steast = "location like 'Steast'" if steast else False
         stwest = "location like 'Stwest'" if stwest else False
-        calories = "calories <= {}".format(calories) if calories != None else True
-        protein = "protein <= {}".format(protein) if protein != None else True
-        fat = "total_fat <= {}".format(fat) if fat != None else True
-        carbs = "total_carbs <= {}".format(carbs) if carbs != None else True
+        calories = f"calories <= {calories}" if calories != None else True
+        protein = f"protein <= {protein}" if protein != None else True
+        fat = f"total_fat <= {fat}" if fat != None else True
+        carbs = f"total_carbs <= {carbs}" if carbs != None else True
         curdate = datetime.now(timezone('US/Eastern'))  # EST timezone
         curdate = curdate.strftime("%Y-%m-%d")
         with engine.connect() as con:
-            foods = con.execute('select distinct * '
-                + "from menu join food_on_menu using (menu_id) "
-                + "join food using (food_id) where menu_date = '{}' ".format(curdate)
-                + "and ({} or {} or {})  ".format(iv, steast, stwest)
-                + "and {} and {} and {} and {} ".format(calories, protein, fat, carbs)
-                + "and meal_type like '{}' ".format(meal)
-                + "and {}".format(foodNameQuery)
-                + "order by location desc, meal_type asc, calories desc, food_name desc")
+            foods = con.execute("select distinct * "
+                f"from menu join food_on_menu using (menu_id) "
+                f"join food using (food_id) where menu_date = '{curdate}' "
+                f"and ({iv} or {steast} or {stwest})  "
+                f"and {calories} and {protein} and {fat} and {carbs} "
+                f"and meal_type like '{meal}' "
+                f"and {foodNameQuery}"
+                f"order by location desc, meal_type asc, calories desc, food_name desc")
         foods = list(foods)
         size = len(foods)
         if size==0:
