d6a41792a1e221476626a6c12900a9dfbb40d828
Vishal Ramesh <vishalramesh50@gmail.com>
2019-04-19 03:10:12 -0400

    limit review to 300 characters

--- dineinhall/reviews/forms.py
+++ dineinhall/reviews/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextAreaField, IntegerField
-from wtforms.validators import DataRequired, Optional, NumberRange
+from wtforms.validators import DataRequired, Optional, NumberRange, Length
 
 
 class ReviewForm(FlaskForm):
     stars = IntegerField('Stars', validators=[DataRequired(), NumberRange(min=1, max=5)])
-    description = TextAreaField('Description', validators=[Optional()])
+    description = TextAreaField('Description', validators=[Optional(), Length(max=300)])
     submit = SubmitField('Submit')
