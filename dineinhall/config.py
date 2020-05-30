b9b37b2ea121f6a84e927b4ccd93bfb29cc21f19
Vishal Ramesh <vishalramesh50@gmail.com>
2020-05-29 21:10:29 -0400

    Use .env instead of creds.py

--- dineinhall/config.py
+++ dineinhall/config.py
@@ -1,19 +1,14 @@
 import os
-try:
-    # os environment variables from Heroku
-    SECRET_KEY = os.environ["SECRET_KEY"]
-    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
-    email_username = os.environ["email_username"]
-    email_password = os.environ["email_password"]
-except Exception:
-    from .creds import SECRET_KEY, SQLALCHEMY_DATABASE_URI, email_username, email_password
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config:
-    SECRET_KEY = SECRET_KEY
-    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = email_username
-    MAIL_PASSWORD = email_password
+    MAIL_USERNAME = os.environ["email_username"]
+    MAIL_PASSWORD = os.environ["email_password"]
